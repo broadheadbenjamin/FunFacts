f4418c367ee490f290dc8e4c0751ec13bef1a313
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-03 14:15:26 -0600

    Initial commit. Files did not originally add.

---
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -0,0 +1,55 @@
+package com.teamtreehouse.funfacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class FunFactsActivity extends Activity {
+
+    private static final String KEY_FACT = "KEY_FACT";
+    private static final String KEY_COLOR = "KEY_COLOR";
+    private TextView mFactLabel;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private String mFact;
+    private int mColor;
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        outState.putString(KEY_FACT, mFact);
+        outState.putInt(KEY_COLOR, mColor);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        mFactLabel = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFact = mFactBook.getFact();
+                mFactLabel.setText(mFact);
+
+                mColor = mColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(mColor);
+                mShowFactButton.setTextColor(mColor);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
